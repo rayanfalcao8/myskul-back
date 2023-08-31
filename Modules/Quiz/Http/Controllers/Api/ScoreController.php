6b18d94b274c27e4784d9c6690b2b75c23db4bbc
Rayan TSOLEFACK <rayanfalcao8@gmail.com>
2023-09-01 00:23:58 +0100

    Score and leaderboard

--- Modules/Quiz/Http/Controllers/Api/ScoreController.php
+++ Modules/Quiz/Http/Controllers/Api/ScoreController.php
@@ -10,22 +10,26 @@ use Modules\User\Entities\User;
 class ScoreController extends CoreController
 {
     public function leaderboard(Request $request) {
+        $users = User::query()
+            ->when($request->level_id, function ($query) use ($request) {
+                return $query->where('level_id', $request->level_id);
+            })
+            ->when($request->speciality_id, function ($query) use ($request) {
+                return $query->where('speciality_id', $request->speciality_id);
+            })
+            ->get()
+            ->map(function ($user) {
+                $user->score = $user->score();
+                return $user;
+            })
+            ->sortByDesc(function ($user) {
+                return $user['score'];
+            })->values();
         return $this->successResponse("Got leaderboard", [
-            'leaderboard' => User::query()
-                ->when($request->level_id, function ($query) use ($request) {
-                    return $query->where('level_id', $request->level_id);
-                })
-                ->when($request->speciality_id, function ($query) use ($request) {
-                    return $query->where('speciality_id', $request->speciality_id);
-                })
-                ->get()->map(function ($user) {
-                    return [
-                        'user' => $user,
-                        'score' => $user->score(),
-                    ];
-                })->sortByDesc(function ($user) {
-                    return $user['score'];
-                })
+            'leaderboard' => $users,
+            'position' => $users->search(function($user) use ($request) {
+                return $user->id === $request->user()->id;
+            }) + 1,
         ]);
     }
     public function score(Request $request) {
